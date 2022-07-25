2283f343c100445bffc36a215f7853752ea638bb
Tkgumbo <tkgumbo@cas.school.nz>
2022-07-25 10:11:20 +0100

    UPDATES

---
+++ ChessTournament/MainClass.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author tgumb
+ */
+import java.util.ArrayList;
+
+public class MainClass {
+    static ArrayList<Players> playernames;
+    List<Player> playername = new ArrayList<Player>();
+
+    String playername = name1.getSelectedText();
+    String playername = name2.getSelectedText();
+    String playername = name3.getSelectedText();
+    String playername = name4.getSelectedText();
+    String playername = name5.getSelectedText();
+    String playername = name6.getSelectedText();
+    String playername = name7.getSelectedText();
+    String playername = name8.getSelectedText();
+    
+    Player playername = new Player();
+    Player.setName(playername); 
+    Player.add(playername);
+  
+}
+
+        
---
+++ ChessTournament/Players.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author tgumb
+ */
+public class Players {
+//INTANCE VERIABLE
+  private String playername;
+
+ //Constructor that takes arguments
+  public Players (String playername){
+    this.playername = playername;
+  }
+//getter
+  public String getPlayername(){
+    return this.playername;
+  }
+//setter
+  public void setplayername(String playername){
+  this.playername = playername;
+  }
+  
+}
+
